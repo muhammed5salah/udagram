c29442d7f731e47e207534a0ea251ee81e9fbfd0
muhammed salah <muhammed5salah@gmail.com>
2022-04-11 23:15:37 +0200

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "Salahapp-env.eba-3nqs8cm8.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://Salahapp-env.eba-3nqs8cm8.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'Salahapp-env.eba-3nqs8cm8.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
